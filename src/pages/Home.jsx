42c053ebf876380115fe0f6885ac3cbbf581aa59
HAL13000 <haru.smile.13@gmail.com>
2024-02-27 22:37:40 +0100

    commit by Haru

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Menu from "../components/common/Menu";
 import "../styles/Home.scss";
 import SearchIcon from "@mui/icons-material/Search";
 import logo_black from "../assets/logo/flavory-high-resolution-logo-black-transparent.png";
 import RecipeBox from "../components/common/RecipeBox";
-// import MenuIcon from "@mui/icons-material/Menu";
-const Home = () => {
-  const [searchRecipe, setSearchRecipe] = useState("");
-  // const [showMenu, setShowMenu] = useState(false);
 
-  const handleSearchRecipe = () => {
-    console.log(searchRecipe);
+const Home = () => {
+  const APP_ID = "0c8eb085";
+  const APP_KEY = "137ccd862a7a33918e3a316299f8f288";
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q="banana"&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.hits);
   };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const [searchRecipe, setSearchRecipe] = useState("");
+
+  const handleSearchRecipe = (e) => {
+    setSearchRecipe(e.target.value);
+  };
+  useEffect(() => {
+    console.log(searchRecipe);
+  }, [searchRecipe]);
 
   return (
     <div className="home">
@@ -24,11 +41,11 @@ const Home = () => {
       <div className="search">
         <input
           type="text"
-          placeholder="Search Recipes"
+          placeholder="I want to cook..."
           value={searchRecipe}
-          onChange={(e) => searchRecipe(e.target.value)}
+          onChange={handleSearchRecipe}
         />
-        <button onClick={handleSearchRecipe}>
+        <button type="submit">
           <SearchIcon />
         </button>
       </div>
