cfe0bde66eb8b674d7fe52b89f837b4c6684235c
HAL13000 <haru.smile.13@gmail.com>
2024-03-02 12:32:19 +0100

    commit by Haruka

--- src/components/common/RecipeBox.jsx
+++ src/components/common/RecipeBox.jsx
@@ -11,6 +11,41 @@ const RecipeBox = () => {
   };
   return (
     <div className="recipeBox">
+      {/* {recipes.map((recipe) => (
+        <div
+          className="recipeBox_container"
+          onClick={navigateToThisRecipe}
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+        >
+          <div className="recipeBox_content">
+            <p className="title">
+              Title Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+              do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p className="caption">
+              Caption Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className="recipeBox_image">
+            <img src={logoOrange} alt="" />
+          </div>
+        </div>
+      ))} */}
       <div className="recipeBox_container" onClick={navigateToThisRecipe}>
         <div className="recipeBox_content">
           <p className="title">
