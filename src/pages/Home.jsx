61c8534f64fdc37b62ac6c8eb1ecf83e5ea7a0c1
HAL13000 <haru.smile.13@gmail.com>
2024-03-13 16:29:02 +0100

    commit by Haru

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Menu from "../components/common/Menu";
 import "../styles/Home.scss";
 import SearchIcon from "@mui/icons-material/Search";
-import logo_black from "../assets/logo/flavory-high-resolution-logo-black-transparent.png";
+// import logo_black from "../assets/logo/flavory-high-resolution-logo-black-transparent.png";
 import RecipeBox from "../components/common/RecipeBox";
 import { useNavigate } from "react-router-dom";
 import recipeApi from "../api/recipeApi";
@@ -12,13 +12,15 @@ import {
   setQueryRecipes,
   setSelectedRecipe,
 } from "../redux/provider/recipeSlice";
+import { useShowMenu } from "../components/common/MenuContext";
 
 const Home = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const recipe = useSelector((state) => state.recipe.setSelectedRecipe);
+  const recipe = useSelector((state) => state.recipe.value);
   const queryRecipes = useSelector((state) => state.recipe.queryRecipes);
   const query = useSelector((state) => state.recipe.query);
+  const { showMenu, handleOpenMenu, handleCloseMenu } = useShowMenu();
 
   const getSearch = async () => {
     try {
@@ -30,10 +32,6 @@ const Home = () => {
       console.log("Error Searching Recipe ", err);
     }
   };
-
-  // useEffect(() => {
-  //   query && getSearch();
-  // }, [query]);
 
   const handleSearchRecipe = (e) => {
     dispatch(setQuery(e.target.value));
@@ -48,26 +46,24 @@ const Home = () => {
   return (
     <div className="home">
       <Menu />
-
-      <div className="logo">
-        <img src={logo_black} alt="Logo" />
-      </div>
-
-      <div className="search">
-        <form>
-          <input
-            type="text"
-            placeholder="I want to cook..."
-            value={query}
-            onChange={handleSearchRecipe}
-          />
-          <button type="button" onClick={getSearch}>
-            <SearchIcon />
-          </button>
-        </form>
-      </div>
-
-      {queryRecipes &&
+      {!showMenu && (
+        <div className="search">
+          <form>
+            <input
+              type="text"
+              placeholder="I want to cook..."
+              value={query}
+              onChange={handleSearchRecipe}
+            />
+            <button type="button" onClick={getSearch}>
+              <SearchIcon />
+            </button>
+          </form>
+        </div>
+      )}
+      {showMenu === true && <div></div>}
+      {!showMenu &&
+        queryRecipes &&
         queryRecipes.map((recipe, i) => (
           <div
             key={i}
@@ -90,26 +86,34 @@ const Home = () => {
 
 export default Home;
 
-// const getSearch = () => {
-//   // ☆
-//   getRecipe(searchRecipe)
-//   recipeApi
-//     .getRecipeFromQuery(searchRecipe)
-//     .then((data) => {
-//       console.log(data);
-//       // update recipes with new data .. data.hits = recipes(useState)
-//       setRecipes(data.hits);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// };
+/* <div className="search">
+  <form>
+    <input
+      type="text"
+      placeholder="I want to cook..."
+      value={query}
+      onChange={handleSearchRecipe}
+    />
+    <button type="button" onClick={getSearch}>
+      <SearchIcon />
+    </button>
+  </form>
+</div> */
 
-// useEffect(() => {
-//   console.log(searchRecipe);
-// }, [searchRecipe]);
-
-// const navigateToRecipe = () => {
-//   navigate("/recipe");
-//   // recipe のページにいく時必ずそのIDと会ったページにいかなきゃいけない
-// };
+/* {queryRecipes &&
+  queryRecipes.map((recipe, i) => (
+    <div
+      key={i}
+      className="recipe"
+      onClick={() => navigateToThisRecipe(recipe.recipe)}
+    >
+      <RecipeBox
+        // !! key
+        // key={recipe.recipe.key}
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+      />
+    </div>
+  ))} */
--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,61 +1,36 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Menu from "../components/common/Menu";
+import "../styles/Recipe.scss";
+import { useNavigate } from "react-router-dom";
 
 const Recipe = () => {
   const selectedRecipe = useSelector((state) => state.recipe.selectedRecipe);
+  const navigate = useNavigate();
+
+  // const OpenOriginalRecipe = () => {
+  //   console.log(selectedRecipe.url);
+  //   navigate(${selectedRecipe.url});
+  // };
   // console.log(selectedRecipe);
   return (
     <section>
       <Menu />
-      <p>{selectedRecipe.label}</p>
-      <img src={selectedRecipe.image} alt="" />
-      <p>{selectedRecipe.ingredientLines}</p>
-      <p>{selectedRecipe.url}</p>
-      <p>{selectedRecipe.calories}</p>
+      <div className="recipe_content">
+        <h2>{selectedRecipe.label}</h2>
+        <img className="recipeImage" src={selectedRecipe.image} alt="" />
+        <p className="ingredientLines">{selectedRecipe.ingredientLines}</p>
+        <div className="recipeUrl">
+          <button
+          // onClick={OpenOriginalRecipe}
+          >
+            Visit{" "}
+          </button>
+        </div>
+        <p className="recipeCalories">{selectedRecipe.calories}</p>
+      </div>
     </section>
   );
 };
 
 export default Recipe;
-
-// import React, { useEffect, useState } from "react";
-// import Menu from "../components/common/Menu";
-// import { useDispatch, useSelector } from "react-redux";
-// import { setRecipeSlice } from "../redux/provider/recipeSlice";
-// import recipeApi from "../api/recipeApi";
-// import { useParams } from "react-router-dom";
-
-// const Recipe = () => {
-//   const query = useParams();
-//   const selectedRecipe = useSelector((state) => state.recipe.selectedRecipe);
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     const fetchRecipeData = async () => {
-//       try {
-//         console.log(selectedRecipe);
-//         const response = await recipeApi.get(query);
-//       } catch (err) {
-//         console.log("Error visiting Recipe ", err);
-//       }
-//     }};
-//     return (
-//       <div>
-//         <Menu />
-//         <h1>{selectedRecipe.label}</h1>
-//       </div>
-//     );
-//   };
-
-// export default Recipe;
-
-//  {/* {/* <div className="recipeData">
-//       <h1 className="title">{recipe.title}</h1>
-//       <ol className="ingredients">
-//         {recipe.ingredients.map((ingredients) => (
-//           <li>{ingredients.text}</li>
-//         ))}
-//       </ol>
-//       <p>{recipe.calories}cal</p>
-//       <img src={recipe.img} alt="recipeImage" /> */}
