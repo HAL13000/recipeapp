260d3d1ed4f986091c04689f63b369918e3fe106
HAL13000 <haru.smile.13@gmail.com>
2024-03-18 18:16:57 +0100

    commit by Haruka

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -3,17 +3,19 @@ import { useSelector } from "react-redux";
 import Menu from "../components/common/Menu";
 import "../styles/Recipe.scss";
 import { useNavigate } from "react-router-dom";
+import { useShowMenu } from "../components/common/MenuContext";
+import Footer from "../components/common/Footer";
 
 const Recipe = () => {
   const selectedRecipe = useSelector((state) => state.recipe.selectedRecipe);
   const navigate = useNavigate();
+  const { showMenu } = useShowMenu();
+  const calories = Math.floor(`${selectedRecipe.calories}`);
 
   const OpenOriginalRecipe = () => {
-    // window.open(`${selectedRecipe.url}`, "_blank");
     window.open(`${selectedRecipe.url}`);
-    //   console.log(selectedRecipe.url);
-    //   navigate(${selectedRecipe.url});
   };
+
   // console.log(selectedRecipe);
   return (
     <section className="recipe">
@@ -24,10 +26,16 @@ const Recipe = () => {
           <img className="recipeImage" src={selectedRecipe.image} alt="" />
 
           <p className="recipe_info">
-            <p className="recipeCalories">
-              Calories : {selectedRecipe.calories}kcal
-            </p>
-            <p className="ingredientLines">{selectedRecipe.ingredientLines}</p>
+            <p className="recipeCalories">Calories : {calories}kcal</p>
+            <div className="ingredientLines">
+              {selectedRecipe.ingredientLines.map((ingredient) => (
+                <p>
+                  {ingredient}
+                  <br />
+                </p>
+              ))}
+            </div>
+            {/* <p className="ingredientLines">{selectedRecipe.ingredientLines}</p> */}
             <div className="recipeUrl">
               <button onClick={OpenOriginalRecipe}>
                 Learn more about this recipe
@@ -36,6 +44,7 @@ const Recipe = () => {
           </p>
         </div>
       </div>
+      {!showMenu && <Footer />}
     </section>
   );
 };
